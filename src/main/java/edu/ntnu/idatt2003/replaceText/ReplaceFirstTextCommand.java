32b9e5bc0d17a86f3371515d91ab8bc852a77f9a
Odin Grav <odin@grav.name>
2026-02-20 12:32:41 +0100

    implemented ReplaceFirstTextCommand class

--- src/main/java/edu/ntnu/idatt2003/replaceText/ReplaceFirstTextCommand.java
+++ src/main/java/edu/ntnu/idatt2003/replaceText/ReplaceFirstTextCommand.java
@@ -1,4 +1,12 @@
 package edu.ntnu.idatt2003.replaceText;
 
-public class ReplaceFIrstTextCommand {
+public class ReplaceFirstTextCommand extends ReplaceTextCommand {
+  ReplaceFirstTextCommand(String target, String replacement) {
+    super(target, replacement);
+  }
+
+  @Override
+  public String exceute(String text) {
+    return text.replaceFirst("(?i)" + target, replacement);
+  }
 }
